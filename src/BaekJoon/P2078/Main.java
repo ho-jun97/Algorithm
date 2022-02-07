e6382f4bbc90f7a0f0bcbd25219a14c259c03f9f
Hojun <hojun9707@naver.com>
2022-02-07 23:16:33 +0900

    [14425번:문자열 집합]-Sliver3

---
+++ src/BaekJoon/P2078/Main.java
@@ -0,0 +1,40 @@
+package BaekJoon.P2078;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ *  https://www.acmicpc.net/problem/2078
+ *  무한이진트리
+ */
+
+public class Main {
+    static int A,B;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        int left = 0;
+        int right = 0;
+        while(A!=0 && B!=0){
+           if(A>B){
+               left += A/B;
+               A = A%B;
+           }else{
+               right += B/A;
+               B = B%A;
+           }
+        }
+        if(A==1){
+            right--;
+        }else{
+            left--;
+        }
+        System.out.println(left + " " + right);
+    }
+}
