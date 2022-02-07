63a1454a948d39ed01121877ffc40f6518a83947
Hojun <hojun9707@naver.com>
2022-02-07 23:20:30 +0900

    [2078번:무한이진트리]-Sliver4

--- src/BaekJoon/P2078/Main.java
+++ src/BaekJoon/P2078/Main.java
@@ -15,7 +15,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         A = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
 
