09b9100fcad6f72c83591955d2ead3052c5e883a
Hojun <hojun9707@naver.com>
2022-02-14 22:11:11 +0900

    [1427번: 소트인사이드]-Sliver5

---
+++ src/BaekJoon/P1427/Main.java
@@ -0,0 +1,27 @@
+package BaekJoon.P1427;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * https://www.acmicpc.net/problem/1427
+ * [1427번: 소트인사이드]-Sliver5
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        PriorityQueue<Integer> list = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int i=0; i<str.length(); i++){
+            list.add(str.charAt(i)-'0');
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!list.isEmpty()) {
+            sb.append(list.poll());
+        }
+    }
+}
