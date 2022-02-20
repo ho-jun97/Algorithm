fc6dd9d5c221a743af63e571bd1c931ebb87c96b
Hojun <hojun9707@naver.com>
2022-02-20 20:25:40 +0900

    [11004번: K번째 수]-Sliver5

---
+++ src/BaekJoon/P11004/Main.java
@@ -0,0 +1,33 @@
+package BaekJoon.P11004;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/1302
+ * [11004번: K번째 수]-Sliver5
+ */
+
+public class Main {
+    static int N,K;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[K-1]);
+    }
+}
