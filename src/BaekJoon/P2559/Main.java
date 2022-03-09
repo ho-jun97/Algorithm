e23f6996c872d972dc1a887ad470859507e3f11c
Hojun <hojun9707@naver.com>
2022-03-09 14:42:31 +0900

    [2559번: 수열]-Sliver3

---
+++ src/BaekJoon/P2559/Main.java
@@ -0,0 +1,38 @@
+package BaekJoon.P2559;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/2559
+ * [2559번: 수열]-Sliver3
+ */
+
+public class Main {
+    static int N,K;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[N+1];
+        int[] arr = new int[N+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            dp[i] = dp[i-1] + arr[i];
+        }
+        int answer = dp[K];
+        for(int i=K; i<=N; i++){
+            answer = Math.max(answer, dp[i]-dp[i-K]);
+        }
+        System.out.println(answer);
+    }
+}
