8d85a0fcec490e573fc5142089ee27c15f4d343d
Hojun <hojun9707@naver.com>
2022-03-15 12:52:15 +0900

    [10773번: 제로]-Sliver4

---
+++ src/BaekJoon/P10773/Main.java
@@ -0,0 +1,35 @@
+package BaekJoon.P10773;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+/**
+ * https://www.acmicpc.net/problem/10773
+ * [10773번: 제로]-Sliver4
+ */
+
+public class Main {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0; i<N; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            if(num==0){
+                stack.pop();
+            }else{
+                stack.push(num);
+            }
+        }
+        int ans = 0;
+        while(!stack.isEmpty()){
+            ans += stack.pop();
+        }
+        System.out.println(ans);
+    }
+}
