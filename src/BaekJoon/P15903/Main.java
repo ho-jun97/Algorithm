72723b5acc2f5d4e1b5f19588e724dc4cc684883
Hojun <hojun9707@naver.com>
2022-03-19 01:46:45 +0900

    [15903번: 카드 합체 놀이]-Sliver2

--- src/BaekJoon/P15903/Main.java
+++ src/BaekJoon/P15903/Main.java
@@ -3,8 +3,6 @@ package BaekJoon.P15903;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
