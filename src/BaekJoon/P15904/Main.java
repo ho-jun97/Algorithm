aa38ef8b02f9f9fe19746977d2c9f71302737e3d
Hojun <hojun9707@naver.com>
2022-03-25 21:05:58 +0900

    [15904번: UCPC는 무엇의 약자일까?]-Sliver5

---
+++ src/BaekJoon/P15904/Main.java
@@ -0,0 +1,33 @@
+package BaekJoon.P15904;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.acmicpc.net/problem/15904
+ * [15904번: UCPC는 무엇의 약자일까?]-Sliver5
+ */
+
+public class Main {
+    static char[] ucpc = {'U','C','P','C'};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+
+        int index=0;
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            if(c==ucpc[index]){
+                index++;
+            }
+            if(index==4) break;
+        }
+        if(index==4){
+            System.out.println("I love UCPC");
+        }else{
+            System.out.println("I hate UCPC");
+        }
+    }
+}
