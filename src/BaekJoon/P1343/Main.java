b31072012a74fd234d19eef8f9422906944799c1
Hojun <hojun9707@naver.com>
2022-03-28 16:31:53 +0900

    [1343번: 폴리오미노]-Sliver5

---
+++ src/BaekJoon/P1343/Main.java
@@ -0,0 +1,26 @@
+package BaekJoon.P1343;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.acmicpc.net/problem/1343
+ * [1343번: 폴리오미노]-Sliver5
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        str = str.replace("XXXX","AAAA");
+        str = str.replace("XX","BB");
+
+        if(str.contains("X")){
+            System.out.println(-1);
+        }else{
+            System.out.println(str);
+        }
+    }
+}
