4a8e19eeabb00e27fc067e407e7d313e8444a242
ho-jun97 <hojun9707@naver.com>
2022-04-06 23:07:44 +0900

    [11508번: 2+1 세일]-Sliver4

---
+++ src/BaekJoon/P11508/Main.java
@@ -0,0 +1,34 @@
+package BaekJoon.P11508;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * https://www.acmicpc.net/problem/11508
+ * [11508번: 2+1 세일]-Sliver4
+ */
+
+public class Main {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        Integer[] list = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(list, Collections.reverseOrder());
+
+        int answer=0;
+        for(int i=0; i<N; i++){
+            if(!(i%3==2)){
+                answer += list[i];
+            }
+        }
+        System.out.println(answer);
+    }
+}
