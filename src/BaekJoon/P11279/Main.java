59c446f3c4029efa7297bd83357e530a64b12c45
ho-jun97 <hojun9707@naver.com>
2022-05-18 22:11:42 +0900

    [11279번: 최대 힙]-Sliver2

---
+++ src/BaekJoon/P11279/Main.java
@@ -0,0 +1,33 @@
+package BaekJoon.P11279;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * https://www.acmicpc.net/problem/11279
+ * [11279번: 최대 힙]-Sliver2
+ */
+
+public class Main {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int i=0; i<N; i++){
+            int n = Integer.parseInt(br.readLine());
+            if(n==0){
+                if(pq.isEmpty()){
+                    System.out.println(0);
+                }else {
+                    System.out.println(pq.poll());
+                }
+            }else{
+                pq.add(n);
+            }
+        }
+    }
+}
