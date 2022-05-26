a6a182eca883ed3e320fb063cfc68092ce3b874d
ho-jun97 <hojun9707@naver.com>
2022-05-26 22:39:17 +0900

    [4949번: 균형잡힌 세상]-Sliver4

---
+++ src/BaekJoon/P4949/Main.java
@@ -0,0 +1,65 @@
+package BaekJoon.P4949;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+/**
+ * https://www.acmicpc.net/problem/4949
+ * [4949번: 균형잡힌 세상]-Sliver4
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String s;
+
+        while(true) {
+            s = br.readLine();
+            if(s.equals(".")) {
+                break;
+            }
+            sb.append(check(s)).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static String check(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(' || c == '[') {
+                stack.push(c);
+            }
+            else if(c == ')') {
+                if(stack.empty() || stack.peek() != '(') {
+                    return "no";
+                }
+                else {
+                    stack.pop();
+                }
+            }
+            else if(c == ']') {
+                if(stack.empty() || stack.peek() != '[') {
+                    return "no";
+                }
+                else {
+                    stack.pop();
+                }
+            }
+        }
+        if(stack.empty()) {
+            return "yes";
+        }
+        else {
+            return "no";
+        }
+    }
+
+}
