6d5775664caa9dffba143e6fce4b6ec270563081
ho-jun97 <hojun9707@naver.com>
2022-10-18 22:53:28 +0900

    (예시 추가)

--- src/Programmers/pccp모의고사2/Main.java
+++ src/Programmers/pccp모의고사2/Main.java
@@ -1,10 +1,12 @@
 package Programmers.pccp모의고사2;
 
-public class Solution {
+import java.io.IOException;
+
+public class Main {
     static int N,M;
     static boolean[] checked;
     static int max = Integer.MIN_VALUE;
-    public int solution(int[][] ability) {
+    public static int solution(int[][] ability) {
         N = ability.length;
         M = ability[0].length;
         checked = new boolean[N];
@@ -12,7 +14,7 @@ public class Solution {
 
         return max;
     }
-    public void dfs(int depth, int sum, int[][] ability){
+    public static void dfs(int depth, int sum, int[][] ability){
         if(depth == M){
             max = Math.max(max, sum);
             return;
@@ -24,5 +26,12 @@ public class Solution {
                 checked[i] = false;
             }
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // 예시
+        int[][] ability = {{40,10,10}, {20,5,0}, {30,30,30} ,{70,0,70} ,{100,100,100}};
+        System.out.println(solution(ability));
+
     }
 }
