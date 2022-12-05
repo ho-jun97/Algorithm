6121dedca2fa1937c2ce7b72cc45c2a028904b5d
ho-jun97 <hojun9707@naver.com>
2022-12-05 23:01:45 +0900

    멀리 뛰기(Lv.2)

---
+++ src/Programmers/멀리뛰기/Solution.java
@@ -0,0 +1,18 @@
+package Programmers.멀리뛰기;
+
+class Solution {
+    public long solution(int n) {
+        long[] dp = new long[n+1];
+
+        if(n==1) return 1;
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1]%1234567 + dp[i-2]%1234567;
+        }
+
+        return dp[n]%1234567;
+    }
+}
