cb501e1f28c5ee64a50e905df3fd1c1efdb08a29
ho-jun97 <hojun9707@naver.com>
2022-12-12 22:58:52 +0900

    2501. Longest Square Streak in an Array
    2501. Longest Square Streak in an Array

---
+++ src/leetCode/Solution.java
@@ -0,0 +1,22 @@
+package leetCode;
+
+import java.util.*;
+class Solution {
+    public int longestSquareStreak(int[] nums) {
+        int max = 1;
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums){
+            set.add(i);
+        }
+        for(int num : set){
+            int count = 1;
+            while(set.contains(num*num)){
+                count++;
+                num *= num;
+            }
+            max = Math.max(max,count);
+        }
+        return max==1?-1:max;
+
+    }
+}
