91f5b8832bea9cbbf909df16296485cb5e380e25
ho-jun97 <hojun9707@naver.com>
2023-02-21 13:12:29 +0900

    점 찍기(Lv.2)

---
+++ src/Programmers/점찍기/Solution.java
@@ -0,0 +1,14 @@
+package Programmers.점찍기;
+
+import java.util.*;
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        for(long i=0; i<=d/k; i++){
+            long x = i*k;
+            long y = (long)Math.sqrt((long)Math.pow(d,2) - (long)Math.pow(x,2))/k;
+            answer += y+1;
+        }
+        return answer;
+    }
+}
