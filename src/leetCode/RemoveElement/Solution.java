9a4273178b84f0a423108a841d4c1186327302f6
ho-jun97 <79128248+ho-jun97@users.noreply.github.com>
2023-06-29 10:48:43 +0900

    Create Solution.java
    27. Remove Element

---
+++ src/leetCode/RemoveElement/Solution.java
@@ -0,0 +1,13 @@
+package leetCode.RemoveElement
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int index = 0;
+        for(int num : nums){
+            if(num != val){
+                nums[index++] = num;
+            }
+        }
+        return index;
+    }
+}
