39f9d549939ccdea864673b1e98336ca88379df0
ho-jun97 <79128248+ho-jun97@users.noreply.github.com>
2023-06-29 14:14:01 +0900

    58. Length of Last Word

--- src/leetCode/leetCode58/Solution.java
+++ src/leetCode/leetCode58/Solution.java
@@ -2,7 +2,7 @@ package leetCode.leetCode58;
 
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] str = s.split("\\s");
+        String[] str = s.trim().split(" ");
         return str[str.length-1].length();
     }
 }
