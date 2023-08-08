d19f6c4c91a825d3c7ccac4fbc686051667a2e59
ho-jun97 <79128248+ho-jun97@users.noreply.github.com>
2023-08-08 14:06:21 +0900

    226. Invert Binary Tree

---
+++ src/leetCode/leetCode226/Solution.java
@@ -0,0 +1,31 @@
+package leetCode.leetCode226;
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+    public static void invert(TreeNode root){
+        if(root == null){
+            return;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invert(root.left);
+        invert(root.right);
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
