8d7e6fe0ecd41cbf212e87da8ff9cbf385b0759f
ho-jun97 <79128248+ho-jun97@users.noreply.github.com>
2023-10-04 17:41:27 +0900

    455. Assign Cookies

---
+++ src/leetCode/leetCode455/Solution.java
@@ -0,0 +1,18 @@
+package leetCode.leetCode455;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int cnt = 0;
+        int i=0, j=0;
+        while(i < g.length && j < s.length){
+            if(g[i] <= s[j]){
+                cnt++;
+                i++;
+            }
+            j++;
+        }
+        return cnt;
+    }
+}
